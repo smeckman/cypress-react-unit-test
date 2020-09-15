4d877d6a6adb1e1d46744c57d06309c5f42ae277
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2020-09-15 16:03:22 +0300

    Remove manual `ReactDom` passing

--- examples/nextjs/cypress/components/Page.spec.jsx
+++ examples/nextjs/cypress/components/Page.spec.jsx
@@ -1,11 +1,10 @@
 import * as React from 'react'
-import * as ReactDom from 'react-dom'
 import IndexPage from '../../pages/index'
 import { mount } from 'cypress-react-unit-test'
 
 describe('NextJS page', () => {
   it('Renders page component', () => {
-    mount(<IndexPage />, { ReactDom })
+    mount(<IndexPage />)
 
     cy.contains('Welcome to Next.js')
   })
--- examples/nextjs/cypress/components/Search.spec.jsx
+++ examples/nextjs/cypress/components/Search.spec.jsx
@@ -1,20 +1,19 @@
 /// <reference types="cypress" />
 import * as React from 'react'
-import * as ReactDom from 'react-dom'
 import { mount } from 'cypress-react-unit-test'
 import { Search } from '../../components/Search'
 import HelloWorld from '../../components/HelloWorld.mdx'
 
 describe('<Search /> NextJS component', () => {
   it('Renders component', () => {
-    mount(<Search />, { ReactDom })
+    mount(<Search />)
 
     cy.get('input').type('124152')
     cy.contains('.search-text', '124152').should('be.visible')
   })
 
   it('Renders mdx component using  custom next.config.js', () => {
-    mount(<HelloWorld />, { ReactDom })
+    mount(<HelloWorld />)
 
     cy.contains('Hello').should('have.css', 'fontWeight', '700')
     cy.contains('This is JSX')
